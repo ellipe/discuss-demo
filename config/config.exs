ce996f073b3196ac788806249c63eb1512b15b1f
Luis Felipe Bertel Mercado <ellipe@gmail.com>
2021-11-05 12:09:58 -0500

    auth package config

--- config/config.exs
+++ config/config.exs
@@ -14,14 +14,19 @@ config :discuss, Discuss.Endpoint,
   url: [host: "localhost"],
   secret_key_base: "Gr/hrGPJClqtNenIQ5MQaawidVYydOooD2SZdAIF/AGDYrCfRx1PTsaxUC5JTyhd",
   render_errors: [view: Discuss.ErrorView, accepts: ~w(html json)],
-  pubsub: [name: Discuss.PubSub,
-           adapter: Phoenix.PubSub.PG2]
+  pubsub: [name: Discuss.PubSub, adapter: Phoenix.PubSub.PG2]
 
 # Configures Elixir's Logger
 config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:request_id]
 
+# Ueberauth
+config :ueberauth, Ueberauth,
+  providers: [
+    github: {Ueberauth.Strategy.Github, []}
+  ]
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
-import_config "#{Mix.env}.exs"
+import_config "#{Mix.env()}.exs"
